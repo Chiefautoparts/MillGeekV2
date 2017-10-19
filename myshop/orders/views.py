baa13003f041219d7b5bbf0245db8230031c5365
chiefautoparts <chiefautoparts@outlook.com>
2017-10-19 16:09:55 -0700

    finsihed order forms

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,6 +1,17 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from .models import OrderItem
+from .forms import OrderCreateForm
+from ..cart.cart import Cart
 
-from django.shortcuts import render
-
-# Create your views here.
+def order_create(request):
+	cart = Cart(request)
+	if request.method == 'POST':
+		form = OrderCreateForm(request.POST)
+		if form.is_valid():
+			order = form.save()
+			for item in cart:
+				OrderItem.objects.create(order=order, product['product'], price=item['price'], quantity=item['quantity'])
+			cart.clear()
+			request.session['order_id'] = order.id
+			return redirect(reverse('payment:process'))
