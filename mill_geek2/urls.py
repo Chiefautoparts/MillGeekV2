57e82be97d73c20c086e6852376c2a73f0c7a6ce
cheifautoparts <chiefautoparts@outlook.com>
2017-10-25 11:10:58 -0700

    custom order_detail admin view

--- mill_geek2/urls.py
+++ mill_geek2/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -26,3 +28,5 @@ urlpatterns = [
     url(r'^payment/', include('myshop.payment.urls', namespace='payments')),
     url(r'^shop/', include('myshop.shop.urls', namespace='shop')),  
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
