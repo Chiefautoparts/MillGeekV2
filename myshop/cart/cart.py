564c7d410d3120098df1263bf2cf106aedfc7c83
cheifautoparts <chiefautoparts@outlook.com>
2017-10-30 12:17:16 -0700

    updates to coupons feature

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -55,3 +55,17 @@ class Cart(object):
 
 	def get_total_price(self):
 		return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+
+	@property
+	def coupon(self):
+		if self.coupon_id:
+			return Coupon.objects.get(id=self.coupon_id)
+		return None
+
+	def get_discount(self):
+		if self.coupon:
+			return (self.coupon.discount / Decimal('100')), self.get_total_price()
+		return Decimal('0')
+
+	def get_total_price_after_discount(self):
+		return self.get_total_price() - self.get_discount()
