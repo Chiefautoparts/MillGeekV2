28292d9ef4192c29ddc1de25d42db5e4639c00bf
cheifautoparts <chiefautoparts@outlook.com>
2017-11-09 18:51:14 -0800

    newsfeed now working

--- home/views.py
+++ home/views.py
@@ -12,7 +12,8 @@ from .models import Post
 def post_list(request):
 	posts = Post.published.all()
 	return render(request, 'home/index.html', {'posts': posts})
-
+def post_detail(request, year, month, day, post):
+	post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
 # def index(request):
 # 	post = NewsFeed.objects.all()
 # 	context = {
@@ -24,19 +25,4 @@ def post_list(request):
 #	conext_object_name = 'posts'
 #	template_name = 'blog/post/list.html'
 # def add_news(request):
-# 	return render(request, 'home/news.html')
-
-#def post_detail(request, year, month, day, post):
-#	post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
-#	return render(request, 'blog/post/detail.html', {'post': post})
-
-# def post_news(request):
-# 	results = NewsFeed.objects.validPost(request.POST)
-# 	if results['status'] is False:
-# 		for error in results['errors']:
-# 			messages.error(request, error)
-# 		return redirect('home:add_news')
-# 	else:
-# 		post = NewsFeed.objects.get.all()
-
-# 	return redirect('home:index')
+# 	return render(request, 'home/news.htm
