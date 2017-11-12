a0cc99aaece6a7de983ba62f8ad94403ba4ef84b
cheifautoparts <chiefautoparts@outlook.com>
2017-11-12 13:59:22 -0800

    picture rendering now

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -4,6 +4,10 @@ from django.core.urlresolvers import reverse
 from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from ..cart.cart import Cart
+from django.template.loader import render_to_string
+import weasyprint
+from django.conf import settings
+from django.http import HttpResponse
 #from .tasks import order_created
 
 
@@ -11,6 +15,14 @@ from ..cart.cart import Cart
 def admin_order_detail(request, order_id):
 	order = get_object_or_404(Order, id=order_id)
 	return render(request, 'admin/orders/order/detail.html', {'order': order})
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+	order = get_object_or_404(Order, id=order_id)
+	html = render_to_string('orders/order/pdf.html', {'order': order})
+	response = HttpResponse(content_type='application/pdf')
+	response['Content-Disposition'] = 'filename'
+	
 def order_create(request):
 	cart = Cart(request)
 	if request.method == 'POST':
@@ -26,12 +38,15 @@ def order_create(request):
 			# else:
 			# 	itemPrice = price
 
+			'''
 			if cart.coupon:
 				order.coupon = cart.coupon
 				order.discount = cart.counpon.discount
 				order.save()
+			'''
 			for item in cart:
-				OrderItem.objects.create(order=order, product=item['product'], price=itemPrice, quantity=item['quantity'])
+				OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+				order.save(commit=False)
 			cart.clear()
 			# launch asynchronous task
 			#order_created.delay(order.id)
@@ -42,3 +57,4 @@ def order_create(request):
 		# order_created.delay(order.id)
 		# request.session['order_id'] = order.id
 		# return redirect(reverse('payments:process'))
+
