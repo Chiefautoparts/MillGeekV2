1685a773a3396183fc0c14f84cf4f30ab00b775e
cheifautoparts <chiefautoparts@outlook.com>
2017-11-16 21:21:07 -0800

    event claendar demo api

--- events/tests.py
+++ events/tests.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 # Create your tests here.
+class SimpleTest(TestCase):
+	self.failUnlessEqual(1 + 1, 2)
+
+__test__ = {"doctest": """
+Another way to test that 1 + 1 is equal to 2.
+
+>>> 1 + 1 == 2
+True
+"""}
