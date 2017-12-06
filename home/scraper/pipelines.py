0af0af1add876b2fd7ce998b2568c00c8b99365c
cheifautoparts <chiefautoparts@outlook.com>
2017-12-06 11:00:00 -0800

    spiders and pipelines

---
+++ home/scraper/pipelines.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+from builtins import str
+from builtins import object
+import logging
+from django.db.utils import IntegrityError
+from scrapy.exceptions import DropItem
+from dynamic_scraper.models import SchedulerRuntime
+
+class DjangoWriterPipeline(object):
+	def process_item(self, item, spider):
+		if spider.conf['DO_ACTION']:
+			try:
+				item['mtgfeed'] = spider.ref_object
+				checker_rt = SchedulerRuntime(runtime_type='C')
+				checker_rt.save()
+				item['checker_runtime'] = checker_rt
+
+				item.save()
+				spider.action_successful = True
+				spider.struct_log("{cs}Item {id} saved to Django DB.{ce}".format(id=item._dds_id_str, cs=spider.bcolors['OK'],ce=spider.bcolors['ENDC']))
+
+			except IntegrityError as e:
+				spider.log(str(e), logging.ERROR)orraise DropItem("Missing Attribute")
+
+		return item
